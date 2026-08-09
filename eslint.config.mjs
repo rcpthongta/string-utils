ab2b7b27921a280a2bc3e8c166721757aa95269e
rcpthongta <220981657+rcpthongta@users.noreply.github.com>
2026-08-09 23:10:33 +0700

    chore(linter): ignore coverage directory in global

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ const jsFiles = ["**/*.cjs", "**/*.js", "**/*.mjs"];
 const tsFiles = ["**/*.ts"];
 
 const configuration = eslint.defineConfig(
-  eslint.globalIgnores(["**/dist/*", "**/node_modules/*", "**/.npmrc", "**/package-lock.json"]),
+  eslint.globalIgnores(["**/coverage/*", "**/dist/*", "**/node_modules/*", "**/.npmrc", "**/package-lock.json"]),
   {
     languageOptions: {
       globals: {
