e3bde5897b6770a597ecf5dad8b88531a1751649
rcpthongta <220981657+rcpthongta@users.noreply.github.com>
2026-08-10 21:52:26 +0700

    feat(libs): add uncapitalize utility function

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,4 +1,6 @@
+export * from "./capitalize.lib";
 export * from "./default-if-blank.lib";
 export * from "./default-if-empty.lib";
 export * from "./has-length.lib";
 export * from "./has-text.lib";
+export * from "./uncapitalize.lib";
