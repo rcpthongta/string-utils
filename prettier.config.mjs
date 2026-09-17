82b1e1a2fdf9e32ecf58cc822d19e3c7e4cacd9d
rcpthongta <220981657+rcpthongta@users.noreply.github.com>
2026-09-18 01:31:35 +0700

    chore(lint): configure eslint and prettier formatting rules

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+const configuration = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  printWidth: 120,
+  quoteProps: "consistent",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+};
+
+export default configuration;
