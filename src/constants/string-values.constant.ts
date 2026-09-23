5724b4952d5fbcd879c6f910f9133a80d9f193cb
rcpthongta <220981657+rcpthongta@users.noreply.github.com>
2026-09-23 14:15:14 +0700

    feat(constants): add DOT, LEFT_BRACE, and RIGHT_BRACE string constants

--- src/constants/string-values.constant.ts
+++ src/constants/string-values.constant.ts
@@ -1,5 +1,8 @@
 export const DOLLAR_SIGN: string = "$";
+export const DOT: string = ".";
 export const EMPTY: string = "";
 export const HYPHEN: string = "-";
+export const LEFT_BRACE: string = "{";
+export const RIGHT_BRACE: string = "}";
 export const SPACE: string = " ";
 export const UNDERSCORE: string = "_";
